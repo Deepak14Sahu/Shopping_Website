e7732d5b1b1327d54da1547895cd74f1d3a115de
Deepak14Sahu <desahu@bestpeers.in>
2023-11-29 13:11:51 +0530

    Add registerUser thunk

--- frontend/src/features/AccountSlice.js
+++ frontend/src/features/AccountSlice.js
@@ -16,6 +16,28 @@ export const loginUser = createAsyncThunk("loginUser", async ({ email, password 
         }
     }
 })
+export const registerUser = createAsyncThunk("registerUser", async ({ name, email, password }, { rejectWithValue }) => {
+    try {
+        const response = await axios.post("http://127.0.0.1:8000/api/account/register/", {
+            name: name,
+            email: email,
+            password: password
+        })
+        return response.data
+    } catch (error) {
+        console.log(error)
+        if (error.response && error.response.status === 401) {
+            return rejectWithValue("Incorrect email or password. Please try again.");
+        } else {
+            return rejectWithValue('Network error. Please try again.');
+        }
+    }
+})
+
+
+
+
+
 
 const AccountSlice = createSlice({
     name: 'account',
@@ -40,6 +62,18 @@ const AccountSlice = createSlice({
                 state.user = action.payload
             })
             .addCase(loginUser.rejected, (state, action) => {
+                state.is_loading = false
+                state.is_error = action.payload
+            })
+            .addCase(registerUser.pending, (state) => {
+                state.is_loading = true
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.is_loading = false
+                state.is_error = null
+                state.user = action.payload
+            })
+            .addCase(registerUser.rejected, (state, action) => {
                 state.is_loading = false
                 state.is_error = action.payload
             })
