b99e2452e8c73289b6006f63388807efc70402c2
Deepak14Sahu <desahu@bestpeers.in>
2023-11-30 18:22:04 +0530

    Add and remove from cart

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('list/', ProductsListView.as_view(), name="ProductList"),
     path('list/<int:pk>/', ProductDetailView.as_view(), name="ProductDetail"),
     path('cart/', ListCart.as_view(), name="CartViewSet"),
-
+    path('cart/<int:pk>/<str:action>/', ListCart.as_view(), name="CartViewSet"),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics
 from .serializers import ProductsListSerializer, CartSerializer
 from .models import ProductList, Cart
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 class ProductsListView(generics.ListCreateAPIView):
     queryset = ProductList.objects.all()
     serializer_class = ProductsListSerializer
@@ -12,16 +13,27 @@ class ProductDetailView(generics.RetrieveAPIView):
     serializer_class = ProductsListSerializer
     lookup_field = 'pk'
     
-class ListCart(generics.RetrieveAPIView):
+class ListCart(generics.RetrieveUpdateAPIView):
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
     def get_object(self):
-        print(self.request.user)
         return Cart.objects.get(user=self.request.user) 
     
     def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
+        cart = self.get_object()
+        serializer = self.get_serializer(cart)
+        return Response(serializer.data)
+    
+    def update(self, request, *args, **kwargs):
+        cart = self.get_object()
+        product_pk = kwargs.get('pk') 
+        product = get_object_or_404(ProductList, pk=product_pk)
+        action = kwargs.get('action')
+        if action == 'add':
+            cart.products.add(product)
+        elif action == 'remove':
+            cart.products.remove(product)
+        serializer = self.get_serializer(cart)
         return Response(serializer.data)
      
         
