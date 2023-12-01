441b3e50fe4d2dcc109f9fc7fb381a868b0b22bd
Deepak14Sahu <desahu@bestpeers.in>
2023-12-01 17:39:45 +0530

    Modify cart backend

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import ProductList, Cart
+from .models import ProductList, Cart, CartProductEntries
 
-# Register your models here.
+
+class CartProductEntryInline(admin.TabularInline):
+    model = CartProductEntries
+    extra = 0
+    readonly_fields = ['subtotal']
+    exclude = ['price']
+    def subtotal(self, obj):
+        return "$" + str(obj.quantity * obj.products.price)
+
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartProductEntryInline]
+    list_display = ['user','get_total_price']
+    def get_total_price(self, obj):
+        return obj.calculate_total_price()
+
+    get_total_price.short_description = 'Total Price'
+    
+    
 admin.site.register(ProductList)
-admin.site.register(Cart)
+admin.site.register(Cart, CartAdmin)
--- backend/product/models.py
+++ backend/product/models.py
@@ -8,14 +8,25 @@ class ProductList(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10 ,decimal_places=2)
     image = models.URLField(blank=True)
+    # quantity        = models.IntegerField()
     
     def __str__(self):
         return self.name
 
+class CartProductEntries(models.Model):
+    cart = models.ForeignKey('Cart', related_name="cart", on_delete=models.CASCADE)
+    products = models.ForeignKey('ProductList', related_name="productlist", on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+    price = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField('ProductList', through='CartProductEntries')
     
-    products = models.ManyToManyField(ProductList)
-    
+    def calculate_total_price(self):
+        total = sum(product.price for product in self.products.all())
+        return total
+
     def __str__(self):
-        return self.user.name + str(self.products.count())
+        return f"{self.user.name}'s Cart"
+
